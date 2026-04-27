9e7c66127e9a4b8076d8d13c97049afcefd467cf
Ruzhan Reaz <154721665+ruzhanreaz@users.noreply.github.com>
2026-04-27 14:20:29 +0600

    Update BackendApplication.java

--- backend/src/main/java/com/vitabridge/backend/BackendApplication.java
+++ backend/src/main/java/com/vitabridge/backend/BackendApplication.java
@@ -3,7 +3,6 @@ package com.vitabridge.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
